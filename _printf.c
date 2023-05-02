ea99929afe34d8252224d0a7f420d634378a75d2
abanoub soliman <bibosoliman20@icloud.com>
2023-05-01 17:28:39 -0700

    task 2

--- _printf.c
+++ _printf.c
@@ -1,49 +1,83 @@
 #include "main.h"
 /**
- * _printf - prints output according to a format
- * @format: the format string
- * Return: the number of characters printed (excluding the null byte)
+ * _printf - prints formatted output to stdout
+ * @format: format string
+ * Return: number of characters printed (excluding null byte)
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int printed_chars = 0;
+	int chars_printed = 0;
 
-	if (format == NULL)
-		return (0);
 	va_start(args, format);
-	for (; *format != '\0'; format++)
+	while (*format != '\0')
 	{
-		if (*format != '%')
+		if (*format == '%')
 		{
-			_putchar(*format);
-			printed_chars++;
-			continue;
+			format++;
+			if (*format == 'c')
+			{
+				int c = va_arg(args, int);
+
+				putchar(c);
+				chars_printed++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char *);
+
+				fputs(s, stdout);
+				chars_printed += strlen(s);
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(args, int);
+
+				printf("%d", num);
+				chars_printed += num_digits(num);
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				chars_printed++;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			chars_printed++;
 		}
 		format++;
-		if (*format == '\0')
-			break;
-		switch (*format)
-		{
-			case 'c':
-				printed_chars += _putchar(va_arg(args, int));
-				break;
-			case 's':
-				printed_chars += print_string(va_arg(args, char *));
-				break;
-			case '%':
-				printed_chars += _putchar('%');
-				break;
-			case 'd':
-			case 'i':
-				printed_chars += print_number(va_arg(args, int));
-				break;
-			default:
-				printf("% %s", format);
-				printed_chars += 2;
-				break;
-		}
 	}
 	va_end(args);
-	return (printed_chars);
+	return (chars_printed);
+}
+
+/**
+ * num_digits - calculates the number of digits in an integer
+ * @num: the integer to check
+ *
+ * Return: the number of digits in the integer
+ */
+int num_digits(int num)
+{
+	int count = 0;
+
+	if (num == 0)
+	{
+		return (1);
+	}
+	else if (num < 0)
+	{
+		count++;
+		num = -num;
+	}
+
+	while (num > 0)
+	{
+		count++;
+		num /= 10;
+	}
+
+	return (count);
 }
